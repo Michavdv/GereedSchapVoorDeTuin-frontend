dcb6331b3a871b71bb73d829f369b77010a0b439
Micha Vegte, van der <s1144423@student.windesheim.nl>
2021-06-29 12:50:58 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>hallo?</h1>
+    <LazyBanner />
+    <LazyApproach />
+    <LazyATM />
+    <img src="~/assets/img/arrow.png" class="arrow-atm-company" />
+    <LazyCompany />
+  </div>
+</template>
+
+<style lang="scss">
+.arrow-atm-company {
+  position: absolute;
+  width: 420px;
+  margin: -180px auto 0 auto;
+  left: 0;
+  right: 0;
+  transform: rotate(10deg);
+}
+
+@media screen and (max-width: 1200px) {
+  .arrow-atm-company {
+    display: none;
+  }
+}
+</style>
